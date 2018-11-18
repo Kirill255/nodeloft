317a455182b5e80d7bc73c15c8270c98154e3b87
Kirill255 <jhgbnm1@mail.ru>
2018-11-18 21:25:49 +0300

    initial

---
+++ README.md
@@ -0,0 +1,92 @@
+# nodeloft
+
+https://habr.com/post/309936/
+
+
+3 основных пакета-конкурента:
+
+https://www.npmtrends.com/commander-vs-minimist-vs-yargs
+
+доп:
+
+https://npms.io/search?q=fs-extra
+
+
+## Run
+
+`node index.js -f=testfolder`
+
+результат примерно такой: (я пока не понял как сделать чтобы вывод был сгруппирован по папкам и файлам, а не в разброс, попробовал несколько способов, рузультат разный, но во всех случаях не тот что нужен)
+
+```
+Работаем!
+File: eee.txt
+Dir: new1
+  File: ooo.txt
+Dir: new2
+  File: ppp.txt
+  File: sss.txt
+Dir: new3
+  Dir: new4
+    Dir: new5
+      File: iii.txt
+      File: uuu.txt
+  File: yyy.txt
+File: rrr.txt
+File: ttt.txt
+Всё сделано!
+```
+желаемый
+
+```
+Работаем!
+Dir: new1
+  File: ooo.txt
+Dir: new2
+  File: ppp.txt
+  File: sss.txt
+Dir: new3
+  Dir: new4
+    Dir: new5
+      File: iii.txt
+      File: uuu.txt
+  File: yyy.txt
+File: eee.txt
+File: rrr.txt
+File: ttt.txt
+Всё сделано!
+```
+
+## Установка eslint
+
+https://eslint.org/docs/user-guide/getting-started
+
+`npm install eslint --save-dev`
+
+https://www.npmjs.com/package/eslint-config-standard
+
+`npm install --save-dev eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node`
+
+`echo "node_modules/" >> .eslintignore`
+
+`touch .eslintrc.json`
+
+with these settings
+
+```json
+{
+  "root": true,
+  "extends": "standard",
+  "rules": {
+    "no-extra-semi": "error",
+    "semi": [
+      "error",
+      "always"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ]
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,126 @@
+// const argv = require("minimist")(process.argv.slice(2), { "--": true, alias: { "o": "output" } });
+const fs = require("fs");
+const path = require("path");
+// const { promisify } = require("util");
+// const asyncReaddir = promisify(fs.readdir);
+// const asyncStat = promisify(fs.stat);
+
+const parseArgs = require("minimist");
+const argv = parseArgs(process.argv.slice(2), {
+  alias: {
+    "f": "folder",
+    "o": "output",
+    "d": "delete"
+  },
+  "--": true
+});
+console.dir(argv);
+
+let level = 0; // уровень вложенности
+
+process.on("exit", code => {
+  // console.log("code :", code);
+  if (code === 0) {
+    console.log("Всё сделано!");
+  }
+  if (code === 1) {
+    console.log("Аргументы обязательны!");
+  }
+  if (code === 2) {
+    console.log("Папки не существует!");
+  }
+});
+
+// синхронно
+const readDir = (basePath, level) => {
+  try {
+    const files = fs.readdirSync(basePath);
+
+    files.forEach(file => {
+      let fullPath = path.join(basePath, file);
+      const stat = fs.statSync(fullPath);
+
+      if (stat.isDirectory()) {
+        console.log(" ".repeat(level) + "Dir: " + file);
+        readDir(fullPath, level + 2); // + 2 пробела, можно и +1, но мне больше нравится +2
+      } else {
+        console.log(" ".repeat(level) + "File: " + file);
+      }
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  // синхронно
+  // try {
+  //   const files = fs.readdirSync(basePath);
+
+  //   for (const file of files) {
+  //     let fullPath = path.join(basePath, file);
+  //     const stat = fs.statSync(fullPath);
+
+  //     if (stat.isDirectory()) {
+  //       console.log(" ".repeat(level) + "Dir: " + file);
+  //       readDir(fullPath, level + 2);
+  //     } else {
+  //       console.log(" ".repeat(level) + "File: " + file);
+  //     }
+  //   }
+  // } catch (err) {
+  //   throw new Error(err);
+  // }
+
+  // асинхронно
+  // fs.readdir(basePath, {}, (err, files) => {
+  //   if (err) {
+  //     throw new Error(err);
+  //   }
+  //   files.forEach(file => {
+  //     let fullPath = path.join(basePath, file);
+  //     fs.stat(fullPath, (err, stat) => {
+  //       if (err) {
+  //         throw new Error(err);
+  //       }
+  //       if (stat.isDirectory()) {
+  //         console.log(" ".repeat(level) + "Dir: " + file);
+  //         readDir(fullPath, level + 2);
+  //       } else {
+  //         console.log(" ".repeat(level) + "File: " + file);
+  //       }
+  //     });
+  //   });
+  // });
+
+  // асинхронно
+  // const readDir = async (basePath, level) => {
+  //   try {
+  //     const files = await asyncReaddir(basePath);
+
+  //     for await (const file of files) {
+  //       let fullPath = path.join(basePath, file);
+  //       const stat = await asyncStat(fullPath);
+
+  //       if (stat.isDirectory()) {
+  //         console.log(" ".repeat(level) + "Dir: " + file);
+  //         readDir(fullPath, level + 2);
+  //       } else {
+  //         console.log(" ".repeat(level) + "File: " + file);
+  //       }
+  //     }
+  //   } catch (err) {
+  //     throw new Error(err);
+  //   }
+};
+
+if (argv["f"] || argv["folder"]) {
+  let userDir = argv["folder"];
+  let basePath = path.join("./", userDir);
+  if (fs.existsSync(basePath)) {
+    console.log("Работаем!");
+    readDir(basePath, level);
+  } else {
+    process.exit(2);
+  }
+} else {
+  process.exit(1);
+}
