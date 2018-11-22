8aac48bec7e703a55f182025f1ad29f40f388ea0
Kirill255 <jhgbnm1@mail.ru>
2018-11-22 14:13:28 +0300

    added a few examples of working with the database

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const db = new Client({
 async function connect () {
   await db.connect();
 
+  // await db.query("INSERT INTO students(s_id, name, start_year) VALUES ($1, $2, $3)", [1567, "Сергей", 2018]); // вставить запись
+  // await db.query("UPDATE students SET start_year = $1 WHERE s_id = $2", [2016, 1567]); // обновить запись
+  // await db.query("DELETE FROM students WHERE s_id = $1", [1567]); // удалить запись
+
   const data = await db.query("SELECT * FROM students");
   // console.log("data.rows :", data.rows);
 
