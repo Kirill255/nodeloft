1af981125e52c6eb4997260ea25272a84de221bd
Kirill255 <jhgbnm1@mail.ru>
2018-11-22 16:02:21 +0300

    add transaction

--- index.js
+++ index.js
@@ -10,16 +10,31 @@ const db = new Client({
 async function connect () {
   await db.connect();
 
-  // await db.query("INSERT INTO students(s_id, name, start_year) VALUES ($1, $2, $3)", [1567, "Сергей", 2018]); // вставить запись
-  // await db.query("UPDATE students SET start_year = $1 WHERE s_id = $2", [2016, 1567]); // обновить запись
-  // await db.query("DELETE FROM students WHERE s_id = $1", [1567]); // удалить запись
+  await db.query("CREATE TABLE groups(g_no text PRIMARY KEY, headman integer NOT NULL REFERENCES students(s_id))"); // создаём новую таблицу
+  await db.query("ALTER TABLE students ADD g_no text REFERENCES groups(g_no)"); // в уже существующую таблицу нужно добавить новый столбец который связывает наши таблицы
 
-  const data = await db.query("SELECT * FROM students");
-  // console.log("data.rows :", data.rows);
+  await db.query("BEGIN"); // начинаем транзакцию
+  await db.query("INSERT INTO groups(g_no, headman) SELECT $1, s_id FROM students WHERE name = $2", ["Loft-05", "Анна"]); // вставляем запись, название группы и староста Анна
+  await db.query("UPDATE students SET g_no = $1", ["Loft-05"]); // переведем всех студентов в созданную группу
+  await db.query("COMMIT"); // завершаем транзакцию
 
-  data.rows.forEach(student => {
-    console.log(`s_id: ${student.s_id} | name: ${student.name} | start_year: ${student.start_year}`);
-  });
+  const students = await db.query("SELECT * FROM students");
+  console.log("students :", students.rows);
+
+  // добавлен новый столбец с названием группы
+  /*
+  [ { s_id: 1451, name: 'Анна', start_year: 2014, g_no: 'Loft-05' },
+  { s_id: 1432, name: 'Виктор', start_year: 2014, g_no: 'Loft-05' },
+  { s_id: 1556, name: 'Нина', start_year: 2015, g_no: 'Loft-05' } ]
+  */
+
+  const groups = await db.query("SELECT * FROM groups");
+  console.log("groups :", groups.rows);
+
+  // создана новая таблица "группы"
+  /*
+  [ { g_no: 'Loft-05', headman: 1451 } ]
+  */
 
   await db.end();
 }
