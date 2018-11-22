1a2f2f981cc5f6545c2f9784ff3b42ae4a19306b
Kirill255 <jhgbnm1@mail.ru>
2018-11-22 13:28:09 +0300

    async/await

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ const db = new Client({
   port: 5432 // это тоже номер порта по умолчанию
 });
 
-db.connect();
+async function connect () {
+  await db.connect();
 
-db.query("SELECT * FROM students", (err, data) => {
-  if (err) {
-    throw new Error(err);
-  }
-  // console.log("data", data); // если мы посмотрим на возвращаемые данные, мы увидим что нам нужно поле rows
+  const data = await db.query("SELECT * FROM students");
   // console.log("data.rows :", data.rows);
 
   data.rows.forEach(student => {
     console.log(`s_id: ${student.s_id} | name: ${student.name} | start_year: ${student.start_year}`);
   });
-  /*
-  s_id: 1451 | name: Анна | start_year: 2014
-  s_id: 1432 | name: Виктор | start_year: 2014
-  s_id: 1556 | name: Нина | start_year: 2015
- */
-  db.end();
-});
+
+  await db.end();
+}
+connect();
